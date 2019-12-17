48da711d9ebb1208483fca6cb928480c20f6c125
Bdani <daniel.berei@consolidity.com>
2019-12-17 13:30:43 +0100

    header logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Header.scss';
 
 const Header = () => {
   return (
-    <div id="header"></div>
+    <div id="header">
+      <div className="logo"><NavLink to='/' exact={true}>scoop</NavLink></div>
+    </div>
   );
 };
 
