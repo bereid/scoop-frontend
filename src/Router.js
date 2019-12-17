b248e46c278cb7819fe049d480244a475bb30562
Bdani <daniel.berei@consolidity.com>
2019-12-17 14:42:51 +0100

    chart package added

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './Layout.js';
+import Expenses from './components/Expenses';
 
 const AppRouter = () => (
     <Router>
         <Layout >
           <Switch>
-          <Route exact path="/expenses"/>
+          <Route exact path="/expenses" component={Expenses}/>
           <Route exact path="/incomes"/>
           <Route exact path="/" />
         </Switch>
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GraphContainer from '../GraphContainer';
+
+import './Expenses.scss';
+
+const Expenses = () => {
+  return (
+    <div id="expenses-container">
+      <GraphContainer />
+    </div>
+  );
+};
+
+export default Expenses;
