d30b593630520a9bfbf0cf3084bffd8f8dd49d76
Berei Dániel <berei.daniel@gmail.com>
2019-12-31 15:10:19 +0100

    deleted unused dependency

--- src/components/GraphContainer/GraphContainer.js
+++ src/components/GraphContainer/GraphContainer.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, RadarChart, RadialChart } from 'react-vis';
 
 import './GraphContainer.scss';
-
-const myData = [{angle: 1}, {angle: 5}, {angle: 2}]
 
 const GraphContainer = () => {
   return (
     <div id="graph-container">
-      <RadialChart
-        data={myData}
-        width={150}
-        height={150} />
     </div>
   );
 };
