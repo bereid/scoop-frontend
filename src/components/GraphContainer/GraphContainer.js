aadefb100b60ac414f3e75fd5f19fae0aa9beffb
Berei Dániel <berei.daniel@gmail.com>
2019-12-31 15:42:52 +0100

    chartcontainer component

--- src/components/GraphContainer/GraphContainer.js
+++ src/components/GraphContainer/GraphContainer.js
@@ -1,10 +1,139 @@
 import React from 'react';
+import { Pie, Doughnut } from 'react-chartjs-2';
 
 import './GraphContainer.scss';
+
+
+const data = {
+  labels: ['January', 'February', 'March',
+    'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      backgroundColor: [
+        '#B21F00',
+        '#C9DE00',
+        '#2FDE00',
+        '#00A6B4',
+        '#6800B4'
+      ],
+      hoverBackgroundColor: [
+        '#501800',
+        '#4B5000',
+        '#175000',
+        '#003350',
+        '#35014F'
+      ],
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
+}
 
 const GraphContainer = () => {
   return (
     <div id="graph-container">
+      <div className="graph-box">
+        <Pie
+          data={data}
+          options={{
+            maintainAspectRatio: false,
+            title: {
+              display: true,
+              text: 'Average Rainfall per month',
+              fontSize: 20
+            },
+            legend: {
+              display: true,
+              position: 'right'
+            }
+          }}
+        />
+      </div>
+      <div className="graph-box">
+      <Doughnut
+          data={data}
+          options={{
+            maintainAspectRatio: false,
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+      </div>
+      <div className="graph-box">
+      <Doughnut
+          data={data}
+          options={{
+            maintainAspectRatio: false,
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+      </div>
+      <div className="graph-box">
+      <Doughnut
+          data={data}
+          options={{
+            maintainAspectRatio: false,
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+      </div>
+      <div className="graph-box">
+      <Doughnut
+          data={data}
+          options={{
+            maintainAspectRatio: false,
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+      </div>
+      <div className="graph-box">
+      <Doughnut
+          data={data}
+          options={{
+            maintainAspectRatio: false,
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+      </div>
     </div>
   );
 };
