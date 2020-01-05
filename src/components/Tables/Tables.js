8b967ed2a36089c7c3f183e15f190d614f6076e7
Berei Dániel <berei.daniel@gmail.com>
2020-01-05 20:48:37 +0100

    nice accordion

--- src/components/Tables/Tables.js
+++ src/components/Tables/Tables.js
@@ -12,9 +12,12 @@ const Tables = (props) => {
     let openTables = [...expandedTables];
     openTables.includes(table) ? openTables.splice(openTables.indexOf(table), 1) : openTables.push(table);
     setExpandedTable(openTables)
+    let isOpen = openTables.includes(table);
+    let expandable = document.querySelector('#' + table);
+    !isOpen ? expandable.style.maxHeight = null : expandable.style.maxHeight = expandable.scrollHeight + 20 + "px";
   }
 
-  const [ checked, setCheck ] = useState([]);
+  const [checked, setCheck] = useState([]);
   const checking = (row) => {
     let checkedRow = [...checked];
     checkedRow.includes(row) ? checkedRow.splice(checkedRow.indexOf(row), 1) : checkedRow.push(row);
@@ -23,22 +26,27 @@ const Tables = (props) => {
 
   const table = (group) => {
     const filteredExpenses = expenses.filter(expense => expense.group === group);
+    let sumOfExpenses = 0;
+    filteredExpenses.forEach(e => sumOfExpenses += e.price);
     const tableNames = {
       bills: "Bills",
       cap: "Expense caps",
       else: "Other expenses"
     }
-  
+
     return (
       <div className="table">
         <div className="table-header" onClick={() => expandTable(group)}>
-          <ColoredIcons>{expandedTables.includes(group) ? <MdExpandLess /> : <MdExpandMore />}</ColoredIcons>
-          {tableNames[group]}
+          <div className="title">
+            <ColoredIcons>{expandedTables.includes(group) ? <MdExpandLess /> : <MdExpandMore />}</ColoredIcons>
+            {tableNames[group]}
+          </div>
+          <span className="sum">{sumOfExpenses}</span>
         </div>
-        <div className={"table-content " + (expandedTables.includes(group) ? "opened" : "")}>
+        <div id={group} className={"table-content " + (expandedTables.includes(group) ? "opened" : "")}>
           {filteredExpenses.map(expense => (
             <div className="table-row" key={expense._id}>
-              <div className="cell icon" onClick={()=>checking(expense._id)}><ColoredIcons>{checked.includes(expense._id) ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}</ColoredIcons></div>
+              <div className="cell icon" onClick={() => checking(expense._id)}><ColoredIcons>{checked.includes(expense._id) ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}</ColoredIcons></div>
               <div className="cell growing">{expense.name}</div>
               <div className="cell wider">{expense.price} HUF</div>
               <div className="cell icon"><ColoredIcons><MdDelete /></ColoredIcons></div>
