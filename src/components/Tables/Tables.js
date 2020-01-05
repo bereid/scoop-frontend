193d733b5211e09f74294b9e5af0475df042cfc2
Berei Dániel <berei.daniel@gmail.com>
2020-01-05 19:30:37 +0100

    tables render dinamically

--- src/components/Tables/Tables.js
+++ src/components/Tables/Tables.js
@@ -1,42 +1,47 @@
 import React, { useState } from 'react';
-import { MdExpandMore, MdExpandLess } from "react-icons/md";
+import { MdExpandMore, MdExpandLess, MdCheckBoxOutlineBlank, MdDelete } from "react-icons/md";
 import ColoredIcons from '../ColoredIcons';
 
 import './Tables.scss';
 
 const Tables = (props) => {
   const { expenses } = props;
-  console.log(expenses);
 
-  const [expandedTable, setExpandedTable] = useState([]);
+  const [expandedTables, setExpandedTable] = useState([]);
 
   const expandTable = (table) => {
-    let openTables = [...expandedTable];
+    let openTables = [...expandedTables];
     openTables.includes(table) ? openTables.splice(openTables.indexOf(table), 1) : openTables.push(table);
     setExpandedTable(openTables)
   }
 
-  console.log(expandedTable)
+  const table = (group) => {
+    const filteredExpenses = expenses.filter(expense => expense.group === group);
+  
+    return (
+      <div className="table">
+        <div className="table-header" onClick={() => expandTable(group)}>
+          <ColoredIcons>{expandedTables.includes(group) ? <MdExpandLess /> : <MdExpandMore />}</ColoredIcons>
+        </div>
+        <div className={"table-content " + (expandedTables.includes(group) ? "opened" : "")}>
+          {filteredExpenses.map(expense => (
+            <div className="table-row" key={expense._id}>
+              <div className="cell icon"><ColoredIcons><MdCheckBoxOutlineBlank /></ColoredIcons></div>
+              <div className="cell growing">{expense.name}</div>
+              <div className="cell wider">{expense.price} HUF</div>
+              <div className="cell icon"><ColoredIcons><MdDelete /></ColoredIcons></div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="table-container">
-      <div className="table-header" onClick={() => expandTable("bills")}>
-        <ColoredIcons>
-          {expandedTable.includes("bills") ? <MdExpandLess /> : <MdExpandMore />}
-        </ColoredIcons>
-        Bills
-        </div>
-      <div className="table-header" onClick={() => expandTable("caps")}>
-        <ColoredIcons>
-          {expandedTable.includes("caps") ? <MdExpandLess /> : <MdExpandMore />}
-        </ColoredIcons>
-        Expense caps
-        </div>
-      <div className="table-header" onClick={() => expandTable("else")}>
-        <ColoredIcons>
-          {expandedTable.includes("else") ? <MdExpandLess /> : <MdExpandMore />}
-        </ColoredIcons>
-        Else
-        </div>
+      {table("bills")}
+      {table("cap")}
+      {table("else")}
     </div >
   );
 };
